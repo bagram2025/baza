7d074dddcac8f3b781e5cf8d47ab1756eae42501
bagram2025 <bagram2025@yandex.com>
2025-10-09 19:20:57 +0300

    Update inst_fastapi.sh
    Подправил

--- telegram_service/inst_fastapi.sh
+++ telegram_service/inst_fastapi.sh
@@ -1,10 +1,9 @@
-cd /home/ubuntuuser/baza/telegram_service
 
 # Создаем виртуальное окружение
 python3 -m venv venv
 source venv/bin/activate
 
 # Устанавливаем зависимости в venv
-pip install fastapi uvicorn httpx
+pip3 install fastapi uvicorn httpx
 
 python3 -c "import fastapi, uvicorn, httpx; print('✅ Все зависимости установлены')"
