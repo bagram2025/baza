e2f523ee6e155f00215bba1c18188242b988bc98
bagram2025 <bagram2025@yandex.com>
2025-10-09 19:36:41 +0300

    Update syst.sh
    Подправил

--- telegram_service/syst.sh
+++ telegram_service/syst.sh
@@ -8,11 +8,11 @@ Type=simple
 User=ubuntuuser
 Group=ubuntuuser
 WorkingDirectory=/home/ubuntuuser/baza/telegram_service
-Environment=TELEGRAM_BOT_TOKEN=6642548401:AAG1df9IFhBijkNYLSVWUPe2FfodsxMOIUg
-Environment=DEFAULT_CHAT_ID=6905230450
-ExecStart=/usr/bin/python3 /home/ubuntuuser/baza/telegram_service/main.py
+Environment=PATH=/home/ubuntuuser/baza/telegram_service/venv/bin:/usr/bin:/usr/local/bin
+ExecStart=/home/ubuntuuser/baza/telegram_service/venv/bin/python3 /home/ubuntuuser/baza/telegram_service/main.py
 Restart=always
 RestartSec=5
+Environment=PYTHONUNBUFFERED=1
 
 [Install]
 WantedBy=multi-user.target
